719bf5c7f22b2dc0a854f6f5296724a62418a044
砂漠の毒虫 <853419196@qq.com>
2019-12-26 18:46:52 +0900

    Delete main.js

--- Node.js/main.js
+++
@@ -1,9 +0,0 @@
-const readline=require("readline").createInterface({input:process.stdin,output:process.stdout});
-readline.on("line",/** @param {String} read */function(read)
-{
-    console.log(read);
-});
-readline.on("close",function()
-{
-    console.log("Hello, World!");
-});
