6280cacf4dd366034b0052a279c137d42c0fc4e6
砂漠の毒虫 <853419196@qq.com>
2020-01-20 02:12:39 +0900

    Add files via upload

---
+++ main.js
@@ -0,0 +1,11 @@
+"use strict";
+const readline=require("readline");
+const stdio=readline.createInterface({input:process.stdin,output:process.stdout});
+stdio.on("line",/** @param {String} read */function(read)
+{
+    console.log(read);
+});
+stdio.on("close",function()
+{
+    console.log("Hello, World!");
+});
