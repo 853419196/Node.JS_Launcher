f58489c5e244bc36d63a3cdbbb6912a1e8f0b35f
砂漠の毒虫 <853419196@qq.com>
2021-06-03 18:53:54 +0900

    Add files via upload

--- Sample/Server.js
+++ Sample/Server.js
@@ -1,7 +1,8 @@
 "use strict";
 const fs=require("fs");
 const http=require("http");
-const rootPath=process.argv[2]||".";
+const port=+process.argv[2]||80;
+const rootPath=process.argv[3]||".";
 const mimeType=
 {
     ".css":"text/css",
@@ -17,18 +18,25 @@ const mimeType=
 }
 http.createServer(function(request,response)
 {
-    let filePath=request.url;
+    const urlPath=decodeURI(request.url);
+    let sliceIndex=urlPath.indexOf("?");
+    if(sliceIndex<0)sliceIndex=urlPath.length;
+    let filePath=urlPath.slice(0,sliceIndex);
+    const queryString=urlPath.slice(sliceIndex+1);
     try
     {
         if(fs.statSync(rootPath+filePath).isDirectory())filePath+="/index.html";
         if(fs.statSync(rootPath+filePath).isFile())
         {
-            let contentType=mimeType[filePath.slice(filePath.lastIndexOf(".")).toLowerCase()]||"application/octet-stream";
+            const contentType=mimeType[filePath.slice(filePath.lastIndexOf(".")).toLowerCase()]||"application/octet-stream";
             response.setHeader("Content-Type",contentType);
             response.writeHead(200);
             fs.createReadStream(rootPath+filePath).pipe(response);
-            console.log("File Path:",filePath);
-            console.log("Content-Type:",contentType);
+            console.log("-".repeat(64));
+            console.log("URL Path:",'"'+urlPath+'"');
+            if(filePath!=urlPath)console.log("File Path:",'"'+filePath+'"');
+            if(queryString)console.log("Query String:",'"'+queryString+'"');
+            console.log("Content-Type:",'"'+contentType+'"');
         }
         else throw null;
     }
@@ -36,8 +44,11 @@ http.createServer(function(request,response)
     {
         response.writeHead(404);
         response.end();
-        console.log("[Error]","File Path:",filePath);
+        console.log("-".repeat(64));
+        console.log("[Error]","URL Path:",'"'+urlPath+'"');
+        if(filePath!=urlPath)console.log("[Error]","File Path:",'"'+filePath+'"');
+        if(queryString)console.log("[Error]","Query String:",'"'+queryString+'"');
     }
-}).listen(80);
-console.log("http://localhost/");
+}).listen(port);
+console.log(port==80?"http://localhost/":"http://localhost:"+port+"/");
 console.log("Root Path:",rootPath);
