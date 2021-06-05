dc1c252624f0f41b121d7bfab681b47263ddd9c6
砂漠の毒虫 <853419196@qq.com>
2021-06-06 03:15:52 +0900

    Add files via upload

--- Sample/HTTPServer.js
+++ Sample/HTTPServer.js
@@ -25,11 +25,11 @@ http.createServer(function(request,response)
 {
     const urlPath=decodeURIComponent(request.url);
     let contentType,pathName,queryString="",sliceIndex=urlPath.indexOf("?");
-    if(sliceIndex<0)pathName=path.posix.normalize(urlPath);
+    if(sliceIndex<0)pathName=path.posix.normalize(urlPath.replace(/\.\./g,""));
     else
     {
         queryString=urlPath.slice(sliceIndex+1);
-        pathName=path.posix.normalize(urlPath.slice(0,sliceIndex));
+        pathName=path.posix.normalize(urlPath.slice(0,sliceIndex).replace(/\.\./g,""));
     }
     try
     {
